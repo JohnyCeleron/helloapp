1ca6d38b0fc838c1f02c60b0c2e60b5b56525955
black <black@example.com>
2023-11-15 12:02:00 +0300

    hello everyone

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -15,7 +15,7 @@ namespace HelloApp
 
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("hello everyone");
         }
     }
 }
